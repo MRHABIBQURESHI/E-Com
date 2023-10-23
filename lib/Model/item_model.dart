beb56a8469d91950b43b957ad726b31279979ad5
mR . habIB qurEshi <105902699+MRHABIBQURESHI@users.noreply.github.com>
2023-10-23 11:05:27 +0500

    Update item_model.dart

--- lib/Model/item_model.dart
+++ lib/Model/item_model.dart
@@ -1,13 +1,13 @@
 class items {
   int id;
   String Name;
-  String image;
+
   int Price;
   int Qty;
   bool IsFav;
 
   items({
-    required this.image,
+ 
     required this.id,
     required this.Name,
     required this.Price,
